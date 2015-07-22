e5191b35076792402be0d39171824f7569da2246
mhat <mhat@mhats-MacBook-Pro.local>
2015-07-22 09:46:15 -0700

    Initial Commit

---
+++ LiteWait/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiteWait
+//
+//  Created by mhat on 7/22/15.
+//  Copyright (c) 2015 mhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
